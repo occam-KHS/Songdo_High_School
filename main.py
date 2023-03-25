f91b0cda5f8c0aaa3f7993de3518ab2fb4979d16
occam-KHS <occam12345@gmail.com>
2023-03-25 23:51:36 +0900

    20230325

---
+++ main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import glob
+import datetime
+
+
+lunch_menus = glob.glob('*중식*.xls')
+dinner_menus = glob.glob('*석식*.xls')
+
+all_orders = pd.read_pickle('all_orders.pkl')
+
+def find_menu(substring):
+
+    menu_list = []
+    type_list = []
+    for lunch_menu in lunch_menus:
+        menu = pd.read_excel(lunch_menu)
+        menu = menu.replace('\n', '', regex=True)
+        menu.replace(np.nan, '', inplace=True)
+        for i, s in menu.iterrows():
+            for j in s:
+                if j != '' and type(j) != int:
+                    if j.find(substring) > 0:
+                        print(lunch_menu.split(' ')[-2], j[:2].replace('[', '') + '일', j)
+                        st.write(lunch_menu.split(' ')[-2] + j[:2].replace('[', '') + '일' + str(j))
+                        menu_list.append(lunch_menu.split(' ')[-2] + j[:2].replace('[', '') + '일')
+                        type_list.append(j.split("[")[1][:2])
+
+    for dinner_menu in dinner_menus:
+        menu = pd.read_excel(dinner_menu)
+        menu = menu.replace('\n', '', regex=True)
+        menu.replace(np.nan, '', inplace=True)
+        for i, s in menu.iterrows():
+            for j in s:
+                if j != '' and type(j) != int:
+                    if j.find(substring) > 0:
+                        print(dinner_menu.split(' ')[-2], j[:2].replace('[', '') + '일', j)
+                        st.write(lunch_menu.split(' ')[-2] + j[:2].replace('[', '') + '일' + str(j))
+                        menu_list.append(dinner_menu.split(' ')[-2] + j[:2].replace('[', '') + '일')
+                        type_list.append(j.split("[")[1][:2])
+
+    yymmdd = [datetime.datetime.strptime(c, "%m월%d일") for c in menu_list]
+    return menu_list, type_list, yymmdd
+
+def find_orders(yymmdd, type_list):
+    find_all = pd.DataFrame()
+    for mmdd, meal in zip(yymmdd, type_list):
+        # print(mmdd.month, '월', mmdd.day,'일', meal)
+        df = all_orders[(all_orders['식사구분'] == meal) & (all_orders['month'] == mmdd.month) & (all_orders['day'] == mmdd.day)]
+        find_all = pd.concat([find_all, df], axis=0)
+    return find_all.drop(columns=['친환경인증정보','원산지','수입국','에듀파인전송여부','menu_date','month','day'])
+
+
+if __name__ == '__main__':
+
+    recipe = st.text_input("**레시피를 입력하세요**")
+
+    if recipe:
+        menu_list, type_list, yymmdd = find_menu(recipe)
+        menu_df = pd.DataFrame({'날짜': menu_list, '식사구분': type_list })
+        st.write(menu_df)
+        df = find_orders(yymmdd, type_list)
+        st.write(df)
